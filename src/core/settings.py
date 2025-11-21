d1e1a4178836fb3fa261f8737f222f29d3e48285
Grigoriy Sokolov <Sokolov_Gr@proton.me>
2025-11-21 23:43:31 +0100

    added basic decorators. deleted db. fixed levellog vug

--- src/core/settings.py
+++ src/core/settings.py
@@ -1,16 +1,13 @@
-import os
-
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = os.getenv("DB_HOST", "db")
-    DB_PORT: int = int(os.getenv("DB_PORT", "5432"))
-    DB_NAME: str = os.getenv("DB_NAME", "reporty")
-    DB_USER: str = os.getenv("DB_USER", "postgres")
-    DB_PASSWORD: str = os.getenv("DB_PASSWORD", "password")
-    LOKI_URL: str = os.getenv("LOKI_URL", "loki_url")
-    LOKI_APP_NAME: str = os.getenv("LOKI_APP_NAME", "loki_app_name")
+    LOKI_URL: str
+    LOKI_APP_NAME: str
+
+    model_config = SettingsConfigDict(
+        env_file="src/.env", env_file_encoding="utf-8", case_sensitive=True, extra="ignore"
+    )
 
 
 settings = Settings()
