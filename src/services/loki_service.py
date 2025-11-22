c719a166325f6e5a71f7307c00c3478996dc903a
Grisha <sokolov_gr@proton.ne>
2025-11-22 09:54:27 +0100

    update level logs for tests

--- src/services/loki_service.py
+++ src/services/loki_service.py
@@ -18,7 +18,7 @@ class LokiService:
     async def get_recent_errors(self, hours: int = 1) -> LokiQueryResult:
         """Получает только ERROR логи за N часов"""
         logger.debug("Calling for get logs by level!")
-        return await self.get_logs_by_level(LogLevel.ERROR, 24)
+        return await self.get_logs_by_level(LogLevel.INFO, 24)
 
     async def get_logs_by_level(self, level: LogLevel, hours: int = 1) -> LokiQueryResult:
         query = f'{{level="{level.value}"}}'
