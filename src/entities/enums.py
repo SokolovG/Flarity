e6acf7a1b86156ba0b2ff5baa4ea9806ef283c4d
Grisha <sokolov_gr@proton.ne>
2025-12-01 17:58:21 +0100

    realized ollama integration

--- src/entities/enums.py
+++ src/entities/enums.py
@@ -12,6 +12,17 @@ class LLMModel(Enum):
     YANDEX_GPT_5 = "yandexgpt"
     ALICE_LLM = "aliceai-llm"
 
+    DEEPSEEK_R1_7B = "deepseek-r1:7b"
+    DEEPSEEK_R1_8B = "deepseek-r1:8b"
+    DEEPSEEK_R1_14B = "deepseek-r1:14b"
+
+    QWEN3_CODER_7B = "qwen3-coder:7b"
+
+    LLAMA_3_2 = "llama3.2"
+    LLAMA_3_2_3B = "llama3.2:3b"
+    MISTRAL = "mistral"
+    GEMMA3_4B = "gemma3:4b"
+
 
 class LogLevel(Enum):
     INFO = "info"
--- src/responses/ollama_responses.py
+++ src/responses/ollama_responses.py
@@ -1,4 +1,19 @@
 from msgspec import Struct
 
 
-class OllamaResponse(Struct): ...
+class OllamaErrorResponse(Struct):
+    error: str
+
+
+class OllamaResponse(Struct):
+    model: str
+    created_at: str
+    response: str
+    done: bool
+    done_reason: str
+    total_duration: int
+    load_duration: int
+    prompt_eval_count: int
+    prompt_eval_duration: int
+    eval_count: int
+    eval_duration: int
