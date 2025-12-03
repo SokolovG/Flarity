e2ac4c143f01f2f796acbc168d7fdc587581408e
Grigoriy Sokolov <Sokolov_Gr@proton.me>
2025-12-03 23:56:10 +0100

    changed settings system

--- src/core/settings/log_source_settings.py
+++ src/core/settings/log_source_settings.py
@@ -2,7 +2,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class LogsSourceSettings(BaseSettings):
-    url: str
-    app_name: str
-
-    model_config = SettingsConfigDict(env_prefix="LOKI_")
+    loki_url: str
+    loki_app_name: str
